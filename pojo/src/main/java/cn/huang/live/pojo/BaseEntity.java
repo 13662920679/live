07a1dae49b1f4b72cfb045c2db5ab16a1505bb8c
黄锡滔 <13662920679@163.com>
2020-12-28 09:15:02 +0800

    整改

--- pojo/src/main/java/cn/huang/live/pojo/BaseEntity.java
+++ pojo/src/main/java/cn/huang/live/pojo/BaseEntity.java
@@ -1,5 +1,6 @@
 package cn.huang.live.pojo;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,16 +11,16 @@ import java.util.Date;
 @Setter
 public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    @ApiModelProperty("主键id")
     private String id;
-
+    @ApiModelProperty("创建时间")
     private Date createTime;
-
+    @ApiModelProperty("创建人id")
     private String createBy;
-
+    @ApiModelProperty("更新时间")
     private Date updateTime;
-
+    @ApiModelProperty("更新人id")
     private String updateBy;
-
+    @ApiModelProperty("启用标识（'0'：未启用；'1'：已启用）")
     private String enableFlag;
 }
--- pojo/src/main/java/cn/huang/live/pojo/sys/SysUserEntity.java
+++ pojo/src/main/java/cn/huang/live/pojo/sys/SysUserEntity.java
@@ -1,17 +1,18 @@
 package cn.huang.live.pojo.sys;
 
 import cn.huang.live.pojo.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class SysUserEntity extends BaseEntity {
-
+    @ApiModelProperty("用户名")
     private String username;
-
+    @ApiModelProperty("密码")
     private String password;
-
+    @ApiModelProperty("角色")
     private String role;
 
 }
